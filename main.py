249ba967d7b266f1118bb33a8fad83d751a36ca8
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2022-06-11 03:37:56 +0500

    get info on the url get client name, count active adv, number of pages to scroll

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import time
 import gspread
 import settings
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 google_account = gspread.service_account(filename=settings.google_json)  # настройка для подключения к гугл таблицам
@@ -12,8 +16,31 @@ current_row = settings.source_start_row  # текущая строка
 # запускаем в работу парсер
 while True:
     time.sleep(1)
-    url = source_worksheet.acell(f'{col}{current_row}').value  # получаем url
-    print(url)
-    if url == settings.source_end_text:  # останавливаем парсер, если записи закончились
+    # получаем url
+    url = source_worksheet.acell(f'{col}{current_row}').value
+
+    # останавливаем парсер, если url больше нет
+    if url == settings.source_end_text:
         break
-    current_row += 1
+
+    # получаем данные с сайта по url
+    while True:
+        try:
+            # задаем браузер и обращаемся к url
+            browser = webdriver.Chrome()
+            browser.get(url)
+            time.sleep(2)
+
+            # получаем имя клиента, количество активных объявлений и страниц
+            client = browser.find_element(By.CLASS_NAME, 'desktop-1tdqab')
+            elem = browser.find_element(By.CLASS_NAME, 'Tabs-nav-tab-title-OGjV6')
+            count_active = int(elem.text.split()[0])  # количество активных объявлений
+            no_of_pagedown = count_active // 16  # количество страниц для прокрутки
+            break
+        except Exception as exc:
+            print(exc)
+            break
+    break
+
+    # берем следующую строку
+    # current_row += 1
