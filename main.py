3115b23b3c28caff3599f246e55e6797e8a2d8c0
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2022-06-11 01:01:40 +0500

    rename start_row to current_row

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import settings
 google_account = gspread.service_account(filename=settings.google_json)  # настройка для подключения к гугл таблицам
 source = google_account.open_by_url(settings.source_sheet_url)  # открывает таблицу с url
 source_worksheet = source.worksheet(settings.source_name_worksheet)  # открываем нужный лист с url
-col = settings.source_col
-start_row = settings.source_start_row
+col = settings.source_col  # колонка, из которой получаем url
+current_row = settings.source_start_row  # текущая строка
 
 # запускаем в работу парсер
 while True:
     time.sleep(1)
-    url = source_worksheet.acell(f'{col}{start_row}').value  # получаем url
+    url = source_worksheet.acell(f'{col}{current_row}').value  # получаем url
     print(url)
     if url == settings.source_end_text:  # останавливаем парсер, если записи закончились
         break
-    start_row += 1
+    current_row += 1
