3f0fc8a9db4f9b328992ba84b704092e6d9f1f2a
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2022-06-11 05:10:44 +0500

    update result sheets

--- main.py
+++ main.py
@@ -12,23 +12,26 @@ source = google_account.open_by_url(settings.source_sheet_url)  # открыва
 source_worksheet = source.worksheet(settings.source_name_worksheet)  # открываем нужный лист с url
 col = settings.source_col  # колонка, из которой получаем url
 current_row = settings.source_start_row  # текущая строка
+current_result_col = settings.result_start_col
 
 # запускаем в работу парсер
 while True:
     time.sleep(5)
     # получаем url
     url = source_worksheet.acell(f'{col}{current_row}').value
-    print(f'Получаем данные по url: {url}')
     # останавливаем парсер, если url больше нет
     if url == settings.source_end_text:
         break
 
+    print(f'Получаем данные по url: {url}')
     # получаем данные с сайта по url
     while True:
         try:
             # задаем браузер и обращаемся к url
-            browser = webdriver.Chrome()
-            browser.get(url)
+            chrome_options = webdriver.ChromeOptions()  # инициализируем опции для Хрома
+            chrome_options.add_argument("--headless")  # добавляем в опции режим headless (без графического интерфейса)
+            browser = webdriver.Chrome(options=chrome_options)  # создаем браузер
+            browser.get(url)  # переходим по ссылке
             time.sleep(2)
 
             # получаем имя клиента, количество активных объявлений и страниц
@@ -42,7 +45,7 @@ while True:
             elem = browser.find_element(By.TAG_NAME, 'body')  # элемент, от которого делаем прокрутку страницы
             while no_of_pagedown:
                 elem.send_keys(Keys.END)
-                time.sleep(0.5)
+                time.sleep(1)
                 no_of_pagedown -= 1
 
             html = browser.page_source  # содержимое страницы
@@ -55,10 +58,25 @@ while True:
                 price = int(raw_price.text.replace('₽', '').replace(' ', ''))
                 prices.append(price)
 
+            # обновляем данные в таблице с ценами
+            print('Обновляем данные по клиенту в таблице с ценами')
+            result_worksheet = source.worksheet(settings.result_name_worksheet)
+            data = [client.text, url]
+            data.extend(prices)
+            row = 1
+            # создаем массив для обновления таблицы и обновляем
+            cells = []
+            for val in data:
+                cells.append(gspread.Cell(row=row, col=current_result_col, value=val))
+                row += 1
+            result_worksheet.update_cells(cells)
+
+            # закрываем браузер и сдвигаем колонку в таблице с результатом
+            browser.close()
+            current_result_col += 1
             break
         except Exception as exc:
             print(exc)
             break
-    break
     # берем следующую строку
-    # current_row += 1
+    current_row += 1
